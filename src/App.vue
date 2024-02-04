ea79a35536a5baba130a7ad5e01f149833616384
yadajen <yada2342547@gmail.com>
2024-02-04 21:37:03 +0700

    fixed bugs (gen Hint)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { ref } from 'vue'
-
-const refreshPage = () => {
-  location.reload() // Reloads the current page
-}
+import { onMounted, ref, toRaw } from 'vue'
+// const refreshPage = () => {
+//   location.reload() // Reloads the current page
+// }
+const start = ref(false)
 
 const missed = ref(0)
+//hint
+const hintsLeft = ref(300)
+let hints = ref([])
+let hintable = ref(false)
+// const timer = ref(0)
+
 //style
 const blockStyle = 'hanjie-cell'
 const noneBorder = 'row-number'
@@ -19,57 +25,135 @@ const blocks = []
 const rows = ['t', '0', '1', '2', '3', '4', '5']
 //columns stores column name of table
 const columns = ['0', 'a', 'b', 'c', 'd', 'e', '99']
-
+//checked blocks array
+const checked = []
+const win = ref(false)
+///times
+const mins = ref(0)
+const secs = ref(0)
+//levels
+const currentLv = ref(0)
 //to stores row and column to blocks
 rows.forEach((ele) => {
   blocks.push({ row: ele, column: [...columns] }) //use spread to easy copy data without reference
 })
-console.log(blocks) //log to show result of block
 
-//result of above
-// blocks = [
-//   {
-//     row: '0',
-//     column: ['0', 'a', 'b', 'c', 'd', 'e', '99'],
-//   },
-//   {
-//     row: '1',
-//     column: ['0', 'a', 'b', 'c', 'd', 'e', '99'],
-//   },
-//   {
-//     row: '2',
-//     column: ['0', 'a', 'b', 'c', 'd', 'e', '99'],
-//   },
-//   {
-//     row: '3',
-//     column: ['0', 'a', 'b', 'c', 'd', 'e', '99'],
-//   },
-//   {
-//     row: '4',
-//     column: ['0', 'a', 'b', 'c', 'd', 'e', '99'],
-//   },
-//   {
-//     row: '5',
-//     column: ['0', 'a', 'b', 'c', 'd', 'e', '99'],
-//   },
-// ]
+//level
+import level from './level.json'
 
 //correctBlock stores block that when click its will change to correct color
-const correctBlock = [
-  'a1',
-  'b1',
-  'e1',
-  'a2',
-  'b2',
-  'c3',
-  'a4',
-  'b4',
-  'c4',
-  'a5',
-  'b5',
-  'c5',
-  'd5',
-]
+let correctBlock = level[currentLv.value].correctBlock
+//headerNums stores id and result of block of table head
+let headerNums = level[currentLv.value].headerNums
+
+let timerInterval
+
+function startGame() {
+  start.value = true
+  timer(true)
+  if (hintsLeft.value >= 0) {
+    hintable.value = true
+  }
+}
+
+function timer(op) {
+  if (op) {
+    timerInterval = setInterval(() => {
+      if (secs.value >= 59) {
+        mins.value++
+        secs.value = 0
+        return
+      } else {
+        secs.value++
+      }
+    }, 1000)
+  } else {
+    if (timerInterval === undefined) {
+      return
+    } else {
+      clearInterval(timerInterval)
+    }
+  }
+}
+//reset block style
+const resetBlockStyles = () => {
+  const allBlocks = document.querySelectorAll('.hanjie-cell')
+  allBlocks.forEach((block) => {
+    block.style.backgroundColor = 'rgba(251,247,245,255)'
+    // block.style.cursor = 'pointer'
+    block.textContent = ''
+  })
+}
+function resetGame() {
+  start.value = false
+  checked.splice(0)
+  win.value = false
+  clearInterval(timerInterval)
+  correctBlock = level[currentLv.value].correctBlock
+  headerNums = level[currentLv.value].headerNums
+  hints.value = []
+}
+
+function nextLevel() {
+  currentLv.value++
+  if (currentLv.value < level.length) {
+    resetGame()
+    resetBlockStyles()
+    startGame()
+    win.value = false
+  } else {
+    alert('Congratulation! You have finished all levels!')
+    currentLv.value = 0
+    resetBlockStyles()
+    resetGame()
+  }
+}
+function checkHintable() {
+  let checkedCorrect = checked.filter((tile) => {
+    return correctBlock.includes(tile)
+  })
+  // let hintAndChecked = hints.value.concat(checkedCorrect)
+  // console.log(hintAndChecked)
+  // if (checked.length >= correctBlock.length) {
+  //   hintable.value = false
+  // }
+  if (hintsLeft.value <= 0) {
+    hintable.value = false
+  }
+  if (checkedCorrect.length === correctBlock.length) {
+    hintable.value = false
+  }
+}
+
+//timer
+// setInterval(() => {
+//   timer.value = timer.value+1
+// }, 1000)
+const playCellElements = ref(null)
+const genHint = () => {
+  let randomIndex
+  if (hintsLeft.value <= 0 || !hintable.value) {
+    return
+  }
+  while (true) {
+    randomIndex = Math.floor(Math.random() * correctBlock.length)
+    if (
+      !checked.includes(correctBlock[randomIndex]) && //หาตัวที่ยังไม่ถูกกด
+      !hints.value.includes(correctBlock[randomIndex]) //และไม่ซ้ำกับ hint ที่กดไปแล้ว
+    ) {
+      hintsLeft.value--
+      hints.value.push(correctBlock[randomIndex])
+      console.log(checked)
+      let press4U = toRaw(playCellElements.value).find(
+        (cellDom) => cellDom.id === correctBlock[randomIndex]
+      )
+      press4U.dispatchEvent(new Event('click'))
+      checkHintable()
+      // checkWin()
+      return
+    }
+  }
+}
 
 //checkHeaderStyle is use for checking that block is header or not to custom style
 const checkHeaderStyle = (id) => {
@@ -78,6 +162,7 @@ const checkHeaderStyle = (id) => {
   if (id.includes('99')) return `${blockStyle} ${noneBorder}`
   return blockStyle
 }
+
 const checkTR = (id) => {
   if (id.includes('0'))
     return `
@@ -87,90 +172,132 @@ const checkTR = (id) => {
   height: 30px;`
 }
 
-//headerNums stores id and result of block of table head
-const headerNums = [
-  {
-    id: 'at',
-    result: '2',
-  },
-  {
-    id: 'bt',
-    result: '2',
-  },
-  {
-    id: 'a0',
-    result: '2',
-  },
-  {
-    id: 'b0',
-    result: '2',
-  },
-  {
-    id: 'c0',
-    result: '3',
-  },
-  {
-    id: 'd0',
-    result: '1',
-  },
-  {
-    id: 'e0',
-    result: '1',
-  },
-  {
-    id: '01',
-    result: '2  1',
-  },
-  {
-    id: '02',
-    result: '2',
-  },
-  {
-    id: '03',
-    result: '1',
-  },
-  {
-    id: '04',
-    result: '3',
-  },
-  {
-    id: '05',
-    result: '4',
-  },
-]
-
 const checkHeader = (id) => {
   const index = headerNums.findIndex((num) => num.id === id) //checking id in array of header numbers to find result
   return index >= 0 ? headerNums[index].result : ''
 }
 
 //addClickers is use to adding click to only block that should be (block that have a line)
-const addClickers = (id) => {
-  if (!id.includes('0') && !id.includes('99')&& !id.includes('t')) {
-    console.log(id)
+const addClickers = (event) => {
+  if (!start.value || win.value) {
+    return
+  }
+  let targetTile = event.target //tile clicked
+  let id = targetTile.id //clicked tile id
+  let targetClasses = targetTile.className.split(' ') //split class into array
+  if (checked.includes(id) || targetClasses.includes('marked')) {
+    return
+  }
+  if (!id.includes('0') && !id.includes('99') && !id.includes('t')) {
     const blockColor = correctBlock.includes(id) ? correct : unCorrect
-    const targetBlock = document.getElementById(id)
+    const targetBlock = targetTile
     targetBlock.style.backgroundColor = blockColor
-    if (blockColor == unCorrect) {
+    if (blockColor === unCorrect) {
       targetBlock.textContent = 'x'
       missed.value++
     }
+    checked.push(id)
+    if (blockColor === correct) {
+      checkHintable()
+    }
+    if (checkWin()) {
+      timer(false)
+    }
   }
 }
+
+function mark(event) {
+  event.preventDefault()
+  let targetTile = event.target //tile clicked
+  let targetTileId = targetTile.id //clicked tile id
+  let targetClasses = targetTile.className.split(' ') //split class into array
+  if (
+    !targetTileId.includes('0') &&
+    !targetTileId.includes('99') &&
+    !targetTileId.includes('t')
+  ) {
+    if (checked.includes(targetTileId)) {
+      //if already checked, return
+      return
+    }
+    if (targetClasses.includes('marked')) {
+      //remove marked class from the tile
+      let markToRm = targetClasses.findIndex((tileClass) => {
+        return tileClass === 'marked'
+      })
+      targetClasses.splice(markToRm, 1)
+      targetTile.className = targetClasses.join(' ')
+      targetTile.style.backgroundColor = 'white'
+      return
+    }
+    targetClasses.push('marked')
+    targetTile.className = targetClasses.join(' ')
+    targetTile.style.backgroundColor = 'grey'
+  }
+}
+
+function checkWin() {
+  let winTemp = true
+  correctBlock.forEach((mustCheckCell) => {
+    if (!checked.includes(mustCheckCell)) {
+      winTemp = false
+    }
+  })
+  console.log(winTemp)
+  win.value = winTemp
+  return winTemp
+}
+
+const show = ref(false)
 </script>
 
 <template>
-  <div class="container p-10 m-auto">
-    <div class="header pb-2 flex justify-center">
-      <div class="mb-4 text-4xl font-extrabold">NUMBER HUNTER</div>
+  <div class="header pb-2 flex justify-center py-3">
+    <div class="mb-4 text-4xl font-extrabold">NUMBER HUNTER</div>
+  </div>
+
+  <!--main tutorial-->
+  <div class="tutorial flex" v-show="!show">
+    <div class="tutorials px-4 py-2 m-2 center">
+      <h1 class="text-center text-2xl font-bold">tutorials</h1>
+      <h2>I think should have some picture</h2>
+      <br /><br /><br /><br />
+      <h2 class="p-20">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+        temporibus, cum harum natus ea dicta sequi accusantium ducimus voluptas
+        deserunt eaque, earum eos. Similique, architecto. Ullam debitis error
+        ipsa unde? Numquam doloribus dolorem aspernatur libero eligendi ab
+        molestias, dolor rem sunt suscipit nihil, totam, ut nulla quae commodi!
+        Quod a vero tempore atque! Perspiciatis, eum nisi quas nihil tempore
+        totam!
+      </h2>
     </div>
-    <button
-      class="btn btn-outline btn-primary"
-      type="reset"
-      @click="refreshPage()"
-    >
-      RESET
+    <button @click="show = !show" class="btn btn-success text-white">
+      Play Game
     </button>
+  </div>
+
+  <div class="container p-10 m-auto w-full" v-show="show">
+    <section class="flex items-center justify-between">
+      <button
+        v-if="!start"
+        class="btn btn-outline btn-primary"
+        type="button"
+        @click="startGame()"
+      >
+        <img src="./assets/play-button.png" class="h-7" />
+        Start
+      </button>
+      <div v-show="start">
+        Time : <span v-if="mins < 10">0</span>{{ mins }} :
+        <span v-if="secs < 10">0</span>{{ secs }}
+      </div>
+      <button v-if="win" class="join-item btn" @click="nextLevel">
+        NEXT LEVEL
+      </button>
+    </section>
+
+    <!--Table-->
     <div class="join flex justify-center">
       <table class="hanjie-table">
         <tr
@@ -180,11 +307,13 @@ const addClickers = (id) => {
           :style="checkTR(block.row)"
         >
           <td
+            ref="playCellElements"
             :class="checkHeaderStyle(col + block.row)"
             v-for="col in block.column"
             :key="col + block.row"
             :id="col + block.row"
-            @click="addClickers(col + block.row)"
+            @click="addClickers"
+            @click.right="mark"
           >
             {{ checkHeader(col + block.row) }}
           </td>
@@ -192,26 +321,39 @@ const addClickers = (id) => {
       </table>
     </div>
 
-    <div class="btm-text flex justify-center content-around">
-      <div class="collapse">
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium justify-start">
-          Hint :
+    <div class="flex justify-between m-5">
+      <!-- Hint -->
+      <div class="hint order-1 flex flex-row">
+        <div class="btn btn-warning m-1 cursor-not-allowed">
+          Hint left : {{ hintsLeft }}
         </div>
-        <div class="collapse-content hidden-hint">
-          <p>hide hint</p>
+        <button
+          :class="
+            hintsLeft > 0 && hintable
+              ? 'btn btn-outline btn-accent m-1'
+              : 'btn btn-active btn-ghost cursor-not-allowed m-1'
+          "
+          :disable="hintsLeft > 0 ? false : true"
+          @click="genHint"
+        >
+          Get hint
+        </button>
+        <div v-if="hints.length > 0" class="px-4 py-2 m-2 font-medium">
+          <b v-for="(hint, index) in hints" :key="hint"
+            >{{ hint }} <b v-if="index < hints.length - 1">, </b></b
+          >
         </div>
       </div>
-
-      <div class="text-xl font-medium justify-items-end">
-        Miss : {{ missed }}
+      <!--Miss-->
+      <div class="missed order-last">
+        <div class="btn m-1 cursor-not-allowed">Missed : {{ missed }}</div>
       </div>
     </div>
 
     <div class="join pagination flex justify-center">
       <button class="join-item btn">«</button>
-      <button class="join-item btn">Level 1</button>
-      <button class="join-item btn">»</button>
+      <button class="join-item btn">Level {{ currentLv }}</button>
+      <button v-if="win" @click="nextLevel" class="join-item btn">»</button>
     </div>
   </div>
 </template>
@@ -228,6 +370,7 @@ const addClickers = (id) => {
   border: 1px solid #000;
   text-align: center;
 }
+
 .hanjie-cell-half {
   width: 70px;
   height: 30px;
@@ -235,6 +378,7 @@ const addClickers = (id) => {
   text-align: center;
   padding-bottom: 10px;
 }
+
 .filled {
   background-color: #000;
 }
