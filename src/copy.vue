09c5128ac94fadc024ea832174d31ef1d87d65e6
xntwpz <110713089+xntwpz@users.noreply.github.com>
2024-02-13 00:41:32 +0700

    add json file

---
+++ src/copy.vue
@@ -0,0 +1,86 @@
+<script setup>
+
+</script>
+ 
+<template>
+<div>
+    <section id="tutorialPage">
+      <!--main tutorial-->
+      <div class="tutorial" v-show="show == 1">
+        <h1 class="text-2xl font-bold p-2 text-center">
+          Tutorial
+        </h1>
+        <section class="parent-section m-2 flex">
+          <div class="box flex flex-col lg:flex-row mx-10 
+          md:mx-20 lg:mx-52 rounded-lg shadow-md ">
+            <!-- page 1-->
+            <div class="change" v-show="tutorial == 0">
+              <img src="./assets/t1-1.png" alt="thumbnail" height="300" width="300" class="thumbnail mx-3">
+              <div class="text-details mx-6">
+                <p class="text-xl  ">Along the top and left side of the grid, there are sequences of
+                  numbers. These numbers provide clues about the groups of filled-in
+                  squares in the corresponding row or column.</p>
+              </div>
+            </div>
+            <!--page 2-->
+            <div class="change" v-show="tutorial == 1">
+              <img src="./assets/t1-2.png" alt="thumbnail" height="300" width="300" class="thumbnail mx-3">
+              <div class="text-details mx-6">
+                <p class="text-xl  ">Each number represents a
+                  consecutive group of filled squares, and the numbers are separated
+                  by at least one blank square.</p>
+              </div>
+            </div>
+            <div class="change" v-show="tutorial == 2">
+              <img src="./assets/t2-1.png" alt="thumbnail" height="300" width="300" class="thumbnail mx-3">
+              <div class="text-details mx-6">
+                <p class="text-xl  ">The order of the numbers corresponds
+                  to the order of the groups in the row or column.</p>
+              </div>
+            </div>
+            <div class="change" v-show="tutorial == 3">
+              <img src="./assets/t2-1.png" alt="thumbnail" height="300" width="300" class="thumbnail mx-3">
+              <div class="text-details mx-6">
+                <p class="text-xl  ">Additionally,
+                  each game mode comes with a timeer to challenge players further.
+                  Players can test their speed-solving skills in various difficulty
+                  levels.</p>
+              </div>
+            </div>
+            <div class="change" v-show="tutorial == 4">
+              <img src="./assets/t2-1.png" alt="thumbnail" height="300" width="300" class="thumbnail mx-3">
+              <div class="text-details mx-6">
+                <p class="text-xl  ">The fastest completion time for each mode will be recorded.
+                  players have access to a total of 3 hints for each level in all mode
+                  to assist them in solving challenging puzzles.</p>
+              </div>
+              <!--button to play game-->
+              <button @click="ezMode" class="btn btn-primary text-white">
+                Easy mode</button><button @click="hardMode" class="btn btn-error text-white">
+                Hard mode
+              </button>
+
+            </div>
+            <div class="next-page-button mt-4">
+              <button v-if="tutorial > 0" @click="beforePage" class="btn btn-outline btn-sm">
+                Previous Page
+              </button>
+              <button v-if="tutorial < 4" @click="nextPage" class="btn btn-inline btn-sm">
+                Next Page
+              </button>
+            </div>
+          </div>
+        </section>
+        <button class="btn btn-outline btn-primary" type="button" @click="homePage">
+          <img src="./assets/Home_icon_green.png" class="h-7" />
+          BACK HOME
+        </button>
+      </div>
+    </section>
+
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
---
+++ src/testCss.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+ 
+<template>
+    <div>
+        <div class=" min-h-screen flex flex-col justify-center items-center">
+            <h1 class="text-4xl text-black mb-8">NUMBER HUNTER</h1>
+            <div class="box-wrapper">
+                <div class="box flex flex-col md:flex-row items-center justify-center py-8 md:py-12">
+                    <div class="md:w-1/2 mb-4 md:mb-0 md:mr-4 mx-3">
+                        <img src="./assets/t1-1.png" alt="Game Image" class="w-full h-auto ">
+                    </div>
+                    <div class="md:w-1/2 flex flex-col items-center justify-center">
+                        <div class="text-lg text-black text-justify mb-4 md:mb-8 p-6">Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Tenetur deserunt asperiores vitae molestiae unde, illo aut suscipit voluptates impedit
+                            voluptatem expedita aperiam libero dolorum porro cumque accusantium error magnam eaque!</div>
+
+                    </div>
+                    <div class="button-group self-start md:self-end flex flex-row  md:flex-col mr-2" >
+                        <button class="btn btn-primary mb-2 m-3">previous page</button>
+                        
+                        <button class="btn btn-primary mb-2 m-3">next page</button>
+                    </div>
+                </div>
+            </div>
+            <div class="p-5">
+              <button class="btn btn-secondary mt-4 ">Home</button>  
+            </div>
+            
+
+        </div>
+
+    </div>
+</template>
+ 
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+
+<style scoped>
+
+.box-wrapper {
+    max-width: 800px;
+}
+
+.box {
+    background-color: #ffffff;
+    /* White background */
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.btn {
+    border-radius: 999px;
+    width: 90% ;
+    margin-top: 3%;
+}
+</style>
